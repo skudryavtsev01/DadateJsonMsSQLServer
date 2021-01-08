a41a184d2746ea4d55a4c57de961eec3893f93dd
Stanislav Kudryavtsev <skudryavtsev@outlook.com>
2021-01-08 19:08:52 +0300

    Добавьте файлы проекта.

---
+++ DadateJsonMsSQLServer/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace DadateJsonMsSQLServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkedListBoxRowsDadata = new System.Windows.Forms.CheckedListBox();
+            this.listBoxRows = new System.Windows.Forms.ListBox();
+            this.buttonGetEmailDB = new System.Windows.Forms.Button();
+            this.buttonSubmitDadate = new System.Windows.Forms.Button();
+            this.buttonUpdateRows = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // checkedListBoxRowsDadata
+            // 
+            this.checkedListBoxRowsDadata.FormattingEnabled = true;
+            this.checkedListBoxRowsDadata.Location = new System.Drawing.Point(417, 12);
+            this.checkedListBoxRowsDadata.Name = "checkedListBoxRowsDadata";
+            this.checkedListBoxRowsDadata.Size = new System.Drawing.Size(363, 259);
+            this.checkedListBoxRowsDadata.TabIndex = 0;
+            // 
+            // listBoxRows
+            // 
+            this.listBoxRows.FormattingEnabled = true;
+            this.listBoxRows.Location = new System.Drawing.Point(12, 12);
+            this.listBoxRows.Name = "listBoxRows";
+            this.listBoxRows.Size = new System.Drawing.Size(361, 264);
+            this.listBoxRows.TabIndex = 1;
+            // 
+            // buttonGetEmailDB
+            // 
+            this.buttonGetEmailDB.Location = new System.Drawing.Point(101, 336);
+            this.buttonGetEmailDB.Name = "buttonGetEmailDB";
+            this.buttonGetEmailDB.Size = new System.Drawing.Size(143, 59);
+            this.buttonGetEmailDB.TabIndex = 2;
+            this.buttonGetEmailDB.Text = "Получить email из БД";
+            this.buttonGetEmailDB.UseVisualStyleBackColor = true;
+            // 
+            // buttonSubmitDadate
+            // 
+            this.buttonSubmitDadate.Location = new System.Drawing.Point(345, 336);
+            this.buttonSubmitDadate.Name = "buttonSubmitDadate";
+            this.buttonSubmitDadate.Size = new System.Drawing.Size(110, 59);
+            this.buttonSubmitDadate.TabIndex = 3;
+            this.buttonSubmitDadate.Text = "Отправить данные в Dadata";
+            this.buttonSubmitDadate.UseVisualStyleBackColor = true;
+            // 
+            // buttonUpdateRows
+            // 
+            this.buttonUpdateRows.Location = new System.Drawing.Point(568, 336);
+            this.buttonUpdateRows.Name = "buttonUpdateRows";
+            this.buttonUpdateRows.Size = new System.Drawing.Size(143, 59);
+            this.buttonUpdateRows.TabIndex = 4;
+            this.buttonUpdateRows.Text = "Изменить строки в БД";
+            this.buttonUpdateRows.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonUpdateRows);
+            this.Controls.Add(this.buttonSubmitDadate);
+            this.Controls.Add(this.buttonGetEmailDB);
+            this.Controls.Add(this.listBoxRows);
+            this.Controls.Add(this.checkedListBoxRowsDadata);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox checkedListBoxRowsDadata;
+        private System.Windows.Forms.ListBox listBoxRows;
+        private System.Windows.Forms.Button buttonGetEmailDB;
+        private System.Windows.Forms.Button buttonSubmitDadate;
+        private System.Windows.Forms.Button buttonUpdateRows;
+    }
+}
+
